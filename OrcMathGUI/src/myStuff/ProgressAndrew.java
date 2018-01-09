47fec276bda75a21b75d1c72a548275c6cf66d0a
Andrew <az@192.168.1.144>
2018-01-08 22:23:17 -0500

    what is this

--- OrcMathGUI/src/myStuff/ProgressAndrew.java
+++ OrcMathGUI/src/myStuff/ProgressAndrew.java
@@ -7,6 +7,9 @@ import guiTeacher.components.*;
 
 public class ProgressAndrew extends Component implements ProgressInterfaceSunny {
 
+	private int round;
+	private int sequenceSize;
+	
 	public ProgressAndrew(int x, int y, int w, int h) {
 		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
@@ -20,7 +23,19 @@ public class ProgressAndrew extends Component implements ProgressInterfaceSunny 
 	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.BLACK);
-		g.drawString("", x, y);
+		g.drawString("", 50, 50);
+	}
+
+	@Override
+	public void setRound(int roundNumber) {
+		// TODO Auto-generated method stub
+		round = roundNumber;
+	}
+
+	@Override
+	public void setSequenceSize(int size) {
+		// TODO Auto-generated method stub
+		sequenceSize = size;
 	}
 
 }
